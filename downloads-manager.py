840155686268e46d00d124249dd5a0db5e864438
xRaptorGG <x.shayan.halder@gmail.com>
2022-07-27 19:39:32 +0530

    Add logging to terminal, bug fixes

--- downloads-manager.py
+++ downloads-manager.py
@@ -1,9 +1,9 @@
 import os
-from os.path import splitext, exists, join
 import shutil
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import logging
 
 # Directories for path and destinations
 path = "C:\\Users\\xshay\\Downloads"
@@ -12,30 +12,32 @@ image_dir = "C:\\Users\\xshay\\Downloads\\Images"
 document_dir = "C:\\Users\\xshay\\Downloads\\Documents"
 software_dir = "C:\\Users\\xshay\\Downloads\\Softwares"
 iso_dir = "C:\\Users\\xshay\\Downloads\\ISOs"
+other_dir = "C:\\Users\\xshay\\Downloads\\Others"
 
 # File extensions
 image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw", ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
 video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg", ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
-document_extensions = [".doc", ".docx", ".odt", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
+document_extensions = [".txt", ".doc", ".docx", ".odt", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
 software_extensions = [".exe", ".msi"]
-iso_extensions = [".iso", ".rar"]
+iso_extensions = [".iso"]
+other_extensions = [".zip", ".rar"]
 
 # Check for duplicate file
 def duplicate(name, destination):
-    f_name, ext = splitext(name);
+    f_name, ext = os.path.splitext(name);
     name = f"{name}1{ext}"
     return name
 
 # Function to move files
 def move(filee, destination, name):
-    if exists(f"{destination}/{name}"):
+    if os.path.exists(f"{destination}/{name}"):
         uniq_name = duplicate(name, destination)
-        old_name = join(destination, name)
-        new_name = join(destination, uniq_name)
-        rename(old_name, new_name)
+        old_name = os.path.join(destination, name)
+        new_name = os.path.join(destination, uniq_name)
+        os.rename(old_name, new_name)
     shutil.move(filee, destination)
 
-# Function to be called when a file is downloaded
+# Event handler class
 class Handler(FileSystemEventHandler):
     def on_modified(self, event):
         with os.scandir(path) as files:
@@ -46,27 +48,38 @@ class Handler(FileSystemEventHandler):
                 self.check_document(filee, name)
                 self.check_software(filee, name)
                 self.check_iso(filee, name)
+                self.check_other(filee, name)
 
     def check_image(self, filee, name):
         for image_extension in image_extensions:
             if name.endswith(image_extension) or name.endswith(image_extension.upper()):
                 move(filee, image_dir, name)
+                logging.info(f"Transfered image {name}")
     def check_video(self, filee, name):
         for video_extension in video_extensions:
             if name.endswith(video_extension) or name.endswith(video_extension.upper()):
                 move(filee, video_dir, name)
+                logging.info(f"Transfered video {name}")
     def check_document(self, filee, name):
         for document_extension in document_extensions:
             if name.endswith(document_extension) or name.endswith(document_extension.upper()):
                 move(filee, document_dir, name)
+                logging.info(f"Transfered document {name}")
     def check_software(self, filee, name):
         for software_extension in software_extensions:
             if name.endswith(software_extension) or name.endswith(software_extension.upper()):
                 move(filee, software_dir, name)
+                logging.info(f"Transfered software {name}")
     def check_iso(self, filee, name):
         for iso_extension in iso_extensions:
             if name.endswith(iso_extension) or name.endswith(iso_extension.upper()):
                 move(filee, iso_dir, name)
+                logging.info(f"Transfered ISO {name}")
+    def check_other(self, filee, name):
+        for other_extension in other_extensions:
+            if name.endswith(other_extension) or name.endswith(other_extension.upper()):
+                move(filee, other_dir, name)
+                logging.info(f"Transfered miscellaneous {name}")
 
 # Event handler
 the_event_handler = Handler()
@@ -75,6 +88,10 @@ the_event_handler = Handler()
 the_observer = Observer()
 the_observer.schedule(the_event_handler, path, recursive=True)
 
+# Logging format
+logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s : %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
 
 # Starting the observer
 the_observer.start()
