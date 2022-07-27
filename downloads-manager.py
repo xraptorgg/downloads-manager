800961b3a4e6be2316340531c843b6a6d2983a52
xRaptorGG <x.shayan.halder@gmail.com>
2022-07-27 17:34:23 +0530

    Add move feature

--- downloads-manager.py
+++ downloads-manager.py
@@ -1,38 +1,54 @@
-import os
+import shutil
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+# Directories for path and destinations
 path = "C:\\Users\\xshay\\Downloads"
+video_dir = "C:\\Users\\xshay\\Downloads\\Videos"
+image_dir = "C:\\Users\\xshay\\Downloads\\Images"
+document_dir = "C:\\Users\\xshay\\Downloads\\Documents"
+software_dir = "C:\\Users\\xshay\\Downloads\\Softwares"
+iso_dir = "C:\\Users\\xshay\\Downloads\\ISOs"
+other_dir = "C:\\Users\\xshay\\Downloads\\Others"
 
-# files = os.listdir(path)
+# File extensions
+image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw", ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
+video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg", ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
+document_extensions = [".doc", ".docx", ".odt", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
+software_extensions = [".exe", ".msi"]
+iso_extensions = [".iso", ".rar"]
 
-# for file in files:
-#     print(file)
+# Function to move files
+def move(source, destination):
+    shutil.move(source, destination)
 
-
-# this is the event handler
+# Event handler
 the_event_handler = FileSystemEventHandler()
 
-
-
-# this is the observer
+# Observer
 the_observer = Observer()
 the_observer.schedule(the_event_handler, path, recursive=False)
 
-
-# function to perform when a file is downloaded
-
+# Function to be called when a file is downloaded
 def on_created(event):
-    print("A file has been downloaded")
+    if event.src_path.endswith in image_extensions:
+        move(event.src_path, image_dir)
+    elif event.src_path.endswith in video_extensions:
+        move(event.src_path, video_dir)
+    elif event.src_path.endswith in document_extensions:
+        move(event.src_path, document_dir)
+    elif event.src_path.endswith in software_extensions:
+        move(event.src_path, software_dir)
+    elif event.src_path.endswith in iso_extensions:
+        move(event.src_path, iso_dir)
+    else:
+        move(event.src_path, other_dir)
 
-
-# calls function when a file is created (downloaded)
-
+# Calls function when a file is downloaded
 the_event_handler.on_created = on_created
 
-# starting the observer
-
+# Starting the observer
 the_observer.start()
 try:
     while True:
