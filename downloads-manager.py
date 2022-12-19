8ca8513a4de15958b72dee521db32a1a981347b1
Bhanu <74284263+Codeptor@users.noreply.github.com>
2022-12-19 12:53:27 +0530

    Added support for Various general File extensions .

--- downloads-manager.py
+++ downloads-manager.py
@@ -18,9 +18,9 @@ other_dir = "C:\\Users\\xshay\\Downloads\\Others"
 image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw", ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
 video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg", ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
 document_extensions = [".txt", ".doc", ".docx", ".odt", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
-software_extensions = [".exe", ".msi"]
-iso_extensions = [".iso"]
-other_extensions = [".zip", ".rar"]
+software_extensions = [".exe", ".msi", ".dll", ".lib"]
+iso_extensions = [".iso", ".img"]
+other_extensions = [".zip", ".rar", ".7z", ".ace"]
 
 # Check for duplicate file
 def duplicate(name, destination):
